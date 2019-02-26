bf5a1ae3f0a8552090fcba25b3f01984e3930814
lsy <18565755413@163.com>
2019-02-26 21:34:58 +0800

    Initial commit

---
+++ 继承.py
@@ -0,0 +1,17 @@
+#!/Users/lsy python
+# coding:utf-8
+# lsy
+
+# 定义汽车类
+class Car():
+    def __init__(self,engine,gasoline):
+        # 初始化属性
+        self.engine = engine
+        self.gasoline = gasoline
+        # 发动机更换方法
+    def engineAsy(self,enginea):
+        self.engine = enginea
+    def print(self):
+        print(self.engine +":"+self.gasoline)
+if __name__ == '__main__':
+    Car('德国','三星').print();
